dc69524810fb7d21633a3dbd046670ad1439f24c
gteimuraz <temogalustian@gmail.com>
2024-08-06 14:28:25 +0400

    Save all

---
+++ cypress/e2e/Class.cy.js
@@ -0,0 +1,10 @@
+describe('template spec', () => {
+    it('passes', () => {
+        let random=Math.round((Math.random()*1000))
+        let email = "test.test"+random+"@gmail.com"
+        let loggingName = "Temo123"+random
+        let password = "12345678"
+        cy.register(email, loggingName, password)
+        cy.log ("is registered")
+    })
+})
---
+++ cypress/support/Login.js
@@ -0,0 +1,14 @@
+Cypress.Commands.add('Login', () => {
+    cy.visit('https://automationteststore.com/')
+
+    let loggingName='Temo123522'
+    let password='12345678'
+
+    cy.get('#customer_menu_top > li > a').click()
+    cy.get('#loginFrm_loginname').type("Temo123522")
+    cy.get('#loginFrm_password').type("12345678")
+    cy.get('#loginFrm > fieldset > .btn').click ()
+    cy.contains("Welcome back Temo").should("be.visible")
+})
+
+//Temo123522   test.test522@gmail.com  12345678
