5b09941c30c2c5f49d2c5b2444a4be79e15b3075
gteimuraz <temogalustian@gmail.com>
2024-08-10 00:02:33 +0400

    Publish

---
+++ cypress/e2e/For HomeWork Cypress3.cy.js/LoginInvalid.cy.js
@@ -0,0 +1,12 @@
+describe('template spec', () => {
+    beforeEach(() => {
+        cy.visit('https://www.automationexercise.com')
+    })
+    it.only('passes', () => {
+        cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
+        cy.get('[data-qa="login-email"]').type("temo.test.6@gmail.com")
+        cy.get('[data-qa="login-password"]').type("temo555")
+        cy.get('[data-qa="login-button"]').click()
+        cy.contains("Your email or password is incorrect!").should("be.visible")
+        })
+    })
--- cypress/e2e/For HomeWork Cypress3.cy.js/LoginInvalid.js
+++
@@ -1,9 +0,0 @@
-describe('template spec', () => {
-    beforeEach(() => {
-        cy.visit('https://www.automationexercise.com')
-    })
-    it.only('passes', () => {
-        cy.get(':nth-child(4) > a').click()
-        
-        })
-    })
