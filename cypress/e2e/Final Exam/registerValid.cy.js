c7e4a3f488040a832dcfda1db50651ef4b579f97
gteimuraz <temogalustian@gmail.com>
2024-09-06 12:55:28 +0400

    save

--- cypress/e2e/Final Exam/registerValid.cy.js
+++ cypress/e2e/Final Exam/registerValid.cy.js
@@ -1,6 +1,6 @@
 describe('Registration with Valid Cridentials', () => {
     it('Register', () => {
         cy.visit('https://testzootopia.loremipsum.ge/')
-        cy.get('[class="iprof"]').click()
+        
     })
 })
