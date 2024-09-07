c916ab971c967211bf26e09873b67265741b8297
gteimuraz <temogalustian@gmail.com>
2024-09-07 18:31:42 +0400

    Sync

--- cypress/e2e/Final Exam/registerValid.cy.js
+++ cypress/e2e/Final Exam/registerValid.cy.js
@@ -1,12 +1,14 @@
-describe('Registration with Valid Cridentials', () => {
+describe('Registration with Valid Cridentials', (email,password) => {
     it('Register', () => {
         cy.visit('https://testzootopia.loremipsum.ge')
+        cy.fixture ("user.json").as("userData");
+        let random = Math.round((Math.random() * 1000))
         cy.get('.menu-pop > .rprof').click()
         cy.get('.input-shablon > p > a').click()
         cy.get(':nth-child(1) > .ismile').type("თემო გალუსტიანი")
-        cy.get('.ipir').type("01010101111")
-        cy.get(':nth-child(2) > .imail').type("temovar@gmail.com")
-        cy.get(':nth-child(4) > .itel').type("597007007")
+        cy.get('.ipir').type(random)
+        cy.get(':nth-child(2) > .imail').type("temovar"+random+"@gmail.com")
+        cy.get(':nth-child(4) > .itel').type("593"+random)
         cy.get(':nth-child(5) > .ipass').type("pass123")
         cy.get('.reg-form-left > :nth-child(6) > .ipass').type("pass123")
         cy.get('[for="profile2"]').click()
